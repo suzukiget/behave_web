a21b444ab7130468d39d322aa591f23dd1e3779a
genasmetchik@gmail.com <genasmetchik@gmail.com>
2018-01-16 13:09:45 +0200

    fix

--- features/steps/step_selenium_alias_hipchat.py
+++ features/steps/step_selenium_alias_hipchat.py
@@ -5,11 +5,10 @@
 # STEPS:
 # ----------------------------------------------------------------------------
 from behave import given, when, then
-import selenium
+from selenium.webdriver.common.by import By
+import time
 
 
-
-'''
 @given('we are on login page')
 def step_impl(context):
     context.login_page.navigate()
@@ -17,23 +16,31 @@ def step_impl(context):
     context.login_page.login()
     context.login_page.enter_pass(context.hipchat_pass)
     context.login_page.login()
-
-
+    assert "Welcome," in context.authorized_page.get_page_head()
 
 @when('we create new Alias')
 def step_impl(context):
-    context.lobby_page.navigate()
-    context.lobby_page.open_chat()
-    context.lobby_page.open_alias_menu()
-    context.lobby_page.pen_drop_down_menu()
-    context.lobby_page.open_menu()
-    context.lobby_page.open_config()
-    context.lobby_page.input_data_in_alias_form()
-    context.lobby_page.input_data_in_alias_name_form()
+
+    context.authorized_page.enter_web_chat()
+    context.chat_page.open_chat()
+    context.chat_page.open_alias_menu()
+    #context.lobby_page.open_drop_down_menu()
+    context.chat_page.open_menu()
+    context.wait.until(lambda driver: driver.find_element_by_class_name('hc-addon-iframe'))
+    #time.sleep(4)
+    context.driver.switch_to.frame(context.driver.find_element_by_class_name('hc-addon-iframe'))
+    try:
+        context.chat_page.open_config()
+    except:
+        context.driver.switch_to.frame(context.driver.find_element_by_class_name('hc-addon-iframe'))
+        context.chat_page.open_config()
+    context.chat_page.input_data_in_alias_form()
+    context.chat_page.input_data_in_alias_name_form()
+
+    pass
 
 
 @then('we check our data')
 def step_impl(context):
-    context.lobby_page.check_input()
+    assert context.chat_page.find_added_el()
 
-'''
