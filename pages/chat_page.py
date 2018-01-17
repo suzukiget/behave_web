07aabe79fc43ff893f7e42ffd869eaba397b7cfc
genasmetchik@gmail.com <genasmetchik@gmail.com>
2018-01-17 02:31:08 +0200

    Fix frame wait

--- pages/chat_page.py
+++ pages/chat_page.py
@@ -33,7 +33,7 @@ class ChatPage(Page):
 
     def open_config(self):
         try:
-            self.context.wait.until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "hc-addon-iframe")))
+            self.context.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "hc-addon-iframe")))
             self.context.driver.switch_to.frame(self.context.driver.find_element_by_class_name('hc-addon-iframe'))
             self.find_config().click()
         except:
